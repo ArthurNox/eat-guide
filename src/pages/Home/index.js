4f0f07c205560334b8e9eda4664274ebba035e49
Arthur Arend <arthur.nox@gmail.com>
2020-02-17 14:31:57 -0300

    Alteraçoes para publicar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,18 +9,97 @@ import logo from '../../assets/logo.svg';
 // import { Container } from './styles';
 
 export default function Home() {
-  const [locations, setLocations] = useState([])
+  const [asdasd, setLocations] = useState([])
 
-  useEffect(() => {
-    async function loadLocations() {
-      const res = await api.get('/locations')
+  // useEffect(() => {
+  //   async function loadLocations() {
+  //     const res = await api.get('/locations')
   
-      setLocations(res.data)
-    }
+  //     setLocations(res.data)
+  //   }
   
-    loadLocations()
+  //   loadLocations()
     
-  }, [])
+  // }, [])
+
+  const locations = [
+    {
+      "name": "Silva Lanches",
+      "menuItens": [
+        {
+          "name": "Xis Bacon",
+          "description": "Pão, alface, tomate, carne, bacon, milho, ervilha, maionese, catchup, mostarda",
+          "price": 20.5
+        },
+        {
+          "name": "Xis Veg",
+          "description": "Pão, alface, tomate, bife de grão-de-bico, milho, ervilha, maionese, catchup, mostarda",
+          "price": 22.5
+        },
+        {
+          "name": "Bauru Picanha",
+          "description": "Pão, alface, tomate, picanha, maionese",
+          "price": 25
+        }
+      ]
+    },
+    {
+      "name": "Z Café",
+      "menuItens": [
+        {
+          "name": "Café Expresso",
+          "description": "50ml do mais puro café moído e passado na hora",
+          "price": 5.5
+        },
+        {
+          "name": "Café Ristretto",
+          "description": "30ml do melhor café aliadodos principais óleos essenciais",
+          "price": 7.5
+        },
+        {
+          "name": "Café Mochaccino",
+          "description": "70ml do melhor café expresso junto ao delicioso leite vaporizado e o melhor chocolate suiço",
+          "price": 10.5
+        }
+      ]
+    },
+    {
+      "name": "Canal Café - PUCRS",
+      "menuItens": [
+        {
+          "name": "Café Expresso",
+          "description": "50ml do puro café moído e passado na hora",
+          "price": 6.5
+        },
+        {
+          "name": "Suco Natural",
+          "description": "500ml de suco de frutas da estação",
+          "price": 12.5
+        }
+      ]
+    },
+    {
+      "name": "Palatu's - PUCRS",
+      "menuItens": [
+        {
+          "name": "Buffet com grelhados",
+          "description": "30 variedades de saladas, pratos quentes e churrasco",
+          "price": 27.5
+        },
+        {
+          "name": "Suco Natural",
+          "description": "500ml de suco de frutas da estação",
+          "price": 4.5
+        },
+        {
+          "name": "Refrigerante Lata",
+          "description": "Pepsi, guaraná, sprite ou sukita",
+          "price": 4.5
+        }
+      ]
+    }
+  ]
+ 
 
 
   return (
